5daac886285423a4a7dea2b5c479b57fb1ceea9f
Jennifer Palacios <jennifer.palacios@parasol-island.com>
2017-11-23 16:51:15 +0100

    update files
    update files

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
-function showErrors(field) {
-  const fieldParent = field.parentNode;
-  const errorField = fieldParent.querySelector('.form--error--message');
+const validateForm = (() => {
+  // Public APIs
+  const publicAPIs = {};
 
-  // Do not do anything if there is a missing error message.
-  if (!errorField) {
-    console.log(`Error message for the required field ${field.name} is missing. Please add an error message.`);
-    return;
-  }
+  const showErrors = (field) => {
+    const fieldParent = field.parentNode;
+    const errorField = fieldParent.querySelector('.form--error--message');
 
-  // If all the error messages are there, we are good to go!
-  errorField.style.opacity = '1';
-
-  // Make sure to put the border only on the right elements
-  if (field.type === 'radio' || field.type === 'checkbox') {
-    return;
-  }
-
-  field.classList.add('form--error--field');
-}
-
-function verifyIsEmpty() {
-  const requiredFields = document.querySelectorAll('.required--field');
-
-  requiredFields.forEach((thisField) => {
-    if (thisField.value === '') {
-      showErrors(thisField);
+    // Do not do anything if there is a missing error message.
+    if (!errorField) {
+      console.log(`Error message for the required field ${field.name} is missing. Please add an error message.`);
+      return;
     }
-  });
-}
 
-function verifyIsChecked(groupName) {
-  const radioButtons = document.getElementsByName(groupName);
+    // If all the error messages are there, we are good to go!
+    errorField.style.opacity = '1';
 
-  radioButtons.forEach((thisRadioButton) => {
-    if (!thisRadioButton.checked) {
-      showErrors(thisRadioButton);
+    // Make sure to put the border only on the right elements
+    if (field.type === 'radio' || field.type === 'checkbox') {
+      return;
     }
-  });
-}
 
-// if (!checkFields()) {
-//   console.log('not good to go!');
-// }
+    field.classList.add('form--error--field');
+  };
 
-// verifyIsEmpty();
-// verifyIsChecked('terms');
-// verifyIsChecked('gender');
+  publicAPIs.verifyIfEmpty = (element) => {
+    const requiredFields = document.querySelectorAll(element);
+
+    requiredFields.forEach((thisField) => {
+      if (thisField.value === '') {
+        showErrors(thisField);
+      }
+    });
+  };
+
+  publicAPIs.verifyIfMarked = (array) => {
+    array.forEach((thisElement) => {
+      const requiredItems = document.getElementsByName(thisElement);
+
+      requiredItems.forEach((thisItem) => {
+        if (!thisItem.checked) {
+          showErrors(thisItem);
+        }
+      });
+    });
+  };
+
+  const runValidator = (event) => {
+    event.preventDefault();
+    // Only run if the form was the right form
+    if (!event.target.classList.contains('avocado-validator'));
+
+    // Get the targeted form
+    publicAPIs.verifyIfEmpty('.required--field');
+    publicAPIs.verifyIfMarked(['terms', 'gender']);
+  };
+
+  // Initialize our plugin
+  publicAPIs.init = () => {
+    document.addEventListener('submit', runValidator);
+  };
+
+  // Return our public APIs
+  return publicAPIs;
+})();
+
+validateForm.init();
